af10d8fa537a90bd92bc590ae9c24eade87d4898
huypham <hoanghuy7284022@gmail.com>
2024-11-14 16:30:26 +0700

    chore rename for clarity

--- src/main/java/engine/constant/Shaders.java
+++ src/main/java/engine/constant/Shaders.java
@@ -3,9 +3,9 @@ package engine.constant;
 import engine.lifecycle.Shader;
 
 public class Shaders {
-    public final static Shader CORE_SHADER = Shader.builder().vertexShaderSource("shader/core_vertex.glsl").fragmentShaderSource("shader/core_fragment.glsl").build();
-    public final static Shader LIGHT_SHADER = Shader.builder().vertexShaderSource("shader/light_vertex.glsl").fragmentShaderSource("shader/light_fragment.glsl").build();
-    public final static Shader MINIMAP_SHADER = Shader.builder().vertexShaderSource("shader/minimap_vertex.glsl").fragmentShaderSource("shader/minimap_fragment.glsl").build();
+    public final static Shader CORE_SHADER = Shader.builder().vertexShaderSource("shader/core.vert").fragmentShaderSource("shader/core.frag").build();
+    public final static Shader LIGHT_SHADER = Shader.builder().vertexShaderSource("shader/light.vert").fragmentShaderSource("shader/light.frag").build();
+    public final static Shader MINIMAP_SHADER = Shader.builder().vertexShaderSource("shader/minimap.vert").fragmentShaderSource("shader/minimap.frag").build();
 
 
     public static void create(){
